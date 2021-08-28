d0c25c9d765c2cdff5b17370ea35f88d2732c430
Ken S <gxcadr@gmail.com>
2021-08-28 00:02:46 -0700

    mostly functionally complete

--- index.js
+++ index.js
@@ -15,15 +15,19 @@ const winMessage = document.getElementById('win');
 
 const winConditions = [
   [square1, square2, square3],
-  [],
-  [],
-  [],
-  [],
-  [],
-  [],
-  []];
+  [square1, square4, square7],
+  [square1, square5, square9],
+  [square2, square5, square8],
+  [square3, square5, square7],
+  [square3, square6, square9],
+  [square4, square5, square6],
+  [square7, square8, square9]];
 
 currentTurn.innerHTML = "X's turn";
+// only begin checking after 5th move
+// draw message
+// fix the algorithm for win condition
+// modify next move text if game ends
 
 function fillSquare(event) {
   console.log('this is the event.target', event.target);
@@ -32,19 +36,27 @@ function fillSquare(event) {
     for (let k = 1; k < winConditions[i].length; k += 1) {
       const currentSquare = winConditions[i][k].children[0].innerHTML; // string
       const prevSquare = winConditions[i][k - 1].children[0].innerHTML; // string
-      if (currentSquare !== prevSquare) {
+      if (currentSquare !== prevSquare || currentSquare === '') {
         break;
       } else if (k === winConditions[i].length - 1) {
         someoneWon = true;
+        setWin();
       }
     }
-  }
-  if (someoneWon) {
-    winMessage.innerHTML = 'Someone won!';
+    if (someoneWon) {
+      break;
+    }
   }
   switchTurn();
   event.target.removeEventListener("click", fillSquare);
   // logic to check if there is a win condition met
+}
+
+function setWin() {
+  winMessage.innerHTML = `${whosTurn} won!`;
+  for (let i = 1; i < 10; i++) {
+    document.getElementById(`square${i}`).removeEventListener("click", fillSquare);
+  }
 }
 
 function switchTurn() {
@@ -59,25 +71,23 @@ function switchTurn() {
 
 document.getElementById("clear").addEventListener('click', clearGame);
 
-function clearGame () {
-  
+function clearGame() {
   currentTurn.innerHTML = "X's turn";
-	const wipeIt = document.querySelectorAll(".square-contents");
+	const allSquares = document.querySelectorAll(".square-contents");
   
   for (let i = 0; i < 9; i++) {
-    wipeIt[i].innerHTML = "";
-    
-  for (let i = 1; i < 10; i++) {
-  
-  document.getElementById(`square${i}`).addEventListener("click", fillSquare);
-  whosTurn = 'X';
-		}
+    allSquares[i].innerHTML = "";
+    document.getElementById(`square${i + 1}`).addEventListener('click', fillSquare);
   }
+
+  winMessage.innerHTML = '';
+  someoneWon = false;
+  whosTurn = 'X';
 }
 
 
 for (let i = 1; i < 10; i++) {
-  document.getElementById(`square${i}`).addEventListener("click", fillSquare);
+  document.getElementById(`square${i}`).addEventListener('click', fillSquare);
 }
 
 
